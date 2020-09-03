429873ef29f3f562c91dfa24c878cdbcd528199c
Saswat <saswatsubhajyotimallick@gmail.com>
2020-09-03 20:39:27 +0530

    additions

--- Sketchy_v2/infer/wgan.py
+++ Sketchy_v2/infer/wgan.py
@@ -394,4 +394,23 @@ def create_model(inputs, targets, net1,net2):
         train=tf.group(update_losses, incr_global_step, gen_train),
     )
 
+def save_images(fetches, step=None):
+    image_dir = os.path.join(a.output_dir, "images")
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
 
+    filesets = []
+    for i, in_path in enumerate(fetches["paths"]):
+        name, _ = os.path.splitext(os.path.basename(in_path.decode("utf8")))
+        fileset = {"name": name, "step": step}
+        for kind in ["inputs", "outputs", "targets"]:
+            filename = name + "-" + kind + ".png"
+            if step is not None:
+                filename = "%08d-%s" % (step, filename)
+            fileset[kind] = filename
+            out_path = os.path.join(image_dir, filename)
+            contents = fetches[kind][i]
+            with open(out_path, "wb") as f:
+                f.write(contents)
+        filesets.append(fileset)
+    return filesets
