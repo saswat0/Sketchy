a18f4faa37245f934898ba582a3cda53f10ce315
Saswat <saswatsubhajyotimallick@gmail.com>
2020-09-03 20:38:46 +0530

    added gen

--- Sketchy_v2/infer/wgan.py
+++ Sketchy_v2/infer/wgan.py
@@ -213,3 +213,70 @@ def load_examples():
         count=len(input_paths),
         steps_per_epoch=steps_per_epoch,
     )
+
+def create_generator(generator_inputs, generator_outputs_channels):
+    layers = []
+
+    # encoder_1: [batch, 256, 256, in_channels] => [batch, 128, 128, ngf]
+    with tf.variable_scope("encoder_1"):
+        output = conv(generator_inputs, a.ngf, stride=2)
+        layers.append(output)
+
+    layer_specs = [
+        a.ngf * 2,
+        a.ngf * 4,
+        a.ngf * 8,
+        a.ngf * 8,
+        a.ngf * 8,
+        a.ngf * 8,
+        a.ngf * 8,
+    ]
+
+    for out_channels in layer_specs:
+        with tf.variable_scope("encoder_%d" % (len(layers) + 1)):
+            rectified = lrelu(layers[-1], 0.2)
+            # [batch, in_height, in_width, in_channels] => [batch, in_height/2, in_width/2, out_channels]
+            convolved = conv(rectified, out_channels, stride=2)
+            output = batchnorm(convolved)
+            layers.append(output)
+
+    layer_specs = [
+        (a.ngf * 8, 0.5),
+        (a.ngf * 8, 0.5),
+        (a.ngf * 8, 0.5),
+        (a.ngf * 8, 0.0),
+        (a.ngf * 4, 0.0),
+        (a.ngf * 2, 0.0),
+        (a.ngf, 0.0),
+    ]
+
+    num_encoder_layers = len(layers)
+    for decoder_layer, (out_channels, dropout) in enumerate(layer_specs):
+        skip_layer = num_encoder_layers - decoder_layer - 1
+        with tf.variable_scope("decoder_%d" % (skip_layer + 1)):
+            if decoder_layer == 0:
+                # first decoder layer doesn't have skip connections
+                # since it is directly connected to the skip_layer
+                input = layers[-1]
+            else:
+                input = tf.concat([layers[-1], layers[skip_layer]], axis=3)
+
+            rectified = tf.nn.relu(input)
+            # [batch, in_height, in_width, in_channels] => [batch, in_height*2, in_width*2, out_channels]
+            output = deconv(rectified, out_channels)
+            output = batchnorm(output)
+
+            if dropout > 0.0:
+                output = tf.nn.dropout(output, keep_prob=1 - dropout)
+
+            layers.append(output)
+
+    # decoder_1: [batch, 128, 128, ngf * 2] => [batch, 256, 256, generator_outputs_channels]
+    with tf.variable_scope("decoder_1"):
+        input = tf.concat([layers[-1], layers[0]], axis=3)
+        rectified = tf.nn.relu(input)
+        output = deconv(rectified, generator_outputs_channels)
+        output = tf.tanh(output)
+        layers.append(output)
+
+    return layers[-1]
