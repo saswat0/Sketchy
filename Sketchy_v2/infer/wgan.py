545d40d5a94de77e8a5e2207fb411b14c093116c
Saswat <saswatsubhajyotimallick@gmail.com>
2020-09-03 20:37:51 +0530

    additions

--- Sketchy_v2/infer/wgan.py
+++ Sketchy_v2/infer/wgan.py
@@ -116,5 +116,17 @@ def deconv(batch_input, out_channels):
                                       [1, 2, 2, 1], padding="SAME")
         return conv
 
+def check_image(image):
+    assertion = tf.assert_equal(tf.shape(image)[-1], 3, message="image must have 3 color channels")
+    with tf.control_dependencies([assertion]):
+        image = tf.identity(image)
 
+    if image.get_shape().ndims not in (3, 4):
+        raise ValueError("image must be either 3 or 4 dimensions")
+
+    # make the last dimension 3 so that you can unstack the colors
+    shape = list(image.get_shape())
+    shape[-1] = 3
+    image.set_shape(shape)
+    return image
 
